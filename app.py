44ac6d96ccd704d54f5b3d26aaf9dc5d57fdfe02
CarolFLima <carolflima0@gmail.com>
2021-07-03 15:58:46 -0300

    adapting to utf8

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+# coding: utf-8
 from flask import Flask, render_template
 from urllib.parse import urlparse
 
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     url = urlparse('apimhmllocal.intranet/api/appointment/v1/searchTimeSlot?addressId=1773&type=452')
     params = url.query.split('&')
 
     return render_template('index.html', params=params)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
