ecc60e69ba7ddd4b1c5cdbb4aa366cf97ee5d271
CarolFLima <carolflima0@gmail.com>
2021-07-06 19:57:34 -0300

    displaying results

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from urllib.parse import urlparse
+
+from flask.globals import request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    url = urlparse('apimhmllocal.intranet/api/appointment/v1/searchTimeSlot?addressId=1773&type=452')
-    params = url.query.split('&')
-
-    return render_template('index.html', params=params)
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        url = urlparse(request.form['url'])
+        params = url.query.split('&')
+        return render_template('index.html', params=params)
 
 
 if __name__ == '__main__':
